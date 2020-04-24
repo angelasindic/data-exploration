7e006769dd3ccd39b65f4cbc2158d9202c05ac80
ailjia <43505441+ailjia@users.noreply.github.com>
2020-04-24 22:28:40 +0200

    Update sediment_over_time.py

--- sediment_over_time.py
+++ sediment_over_time.py
@@ -1,26 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import os
 import glob
 import pandas as pd
 from netCDF4 import Dataset
 
-df = pd.DataFrame()
-name_list = []
-for folder_name in os.listdir():
-    name_list.append(folder_name)
-    df['date'] = name_list
-    for file_name in glob.glob(os.getcwd()+ '/' + folder_name + '/*L2W.nc'):
-        nc = Dataset(file_name)
-        ds = list(nc.variables.keys())
-        dataset = ds[3] # spm_nechad2016 represents the sedimentaiton density
-        data_temp = nc.variables[dataset][:]
-        sediment = data_temp[data_temp>10]
-        df['sediment (km2)'] = len(sediment) * 100 / 1000000 
-        
-df.to_csv('sediment_over_time.csv', index=False)
+df = pd.DataFrame(columns=['date', 'sediment(km2)'])
+df_temp = pd.DataFrame(columns=['date', 'sediment(km2)'])
 
+for folder_name in os.listdir():
+    if '-' in folder_name: 
+        for file_name in glob.glob(os.getcwd()+ '/' + folder_name + '/*L2W.nc'):
+            nc = Dataset(file_name)
+            ds = list(nc.variables.keys())
+            dataset = ds[3] # spm_nechad2016 represents the sedimentaiton density
+            data_temp = nc.variables[dataset][:]
+            sediment = data_temp[data_temp>10]
+            df_temp.loc[1] = [folder_name] + [len(sediment)*100/1000000]
+            df = pd.concat([df, df_temp])
+            
