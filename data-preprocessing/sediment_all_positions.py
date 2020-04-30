ad4be4e187c717356750f0a847a71ea117844e1a
ailjia <43505441+ailjia@users.noreply.github.com>
2020-05-01 00:20:16 +0200

    # a much faster code

--- data-preprocessing/sediment_all_positions.py
+++ data-preprocessing/sediment_all_positions.py
@@ -8,21 +8,20 @@ import pandas as pd
 from netCDF4 import Dataset
 import numpy as np
 
-df_temp = pd.DataFrame(columns=['sediment(km2)'])
 df = pd.DataFrame()
-
 for folder_name in os.listdir():
     if '-' in folder_name: 
         for file_name in glob.glob(os.getcwd()+ '/' + folder_name + '/*L2W.nc'):
             nc = Dataset(file_name)
             ds = list(nc.variables.keys())
             dataset = ds[3] 
-            data_temp = nc.variables[dataset][:][500:800, 1200:1500] #[300:500, 1200:1500] #select a small part close to the coast
-            data = data_temp.flatten('C') #‘C’ means to flatten in row-major (C-style) order
-            df_temp.loc[1] = [data]
-            df_new = pd.DataFrame(df_temp['sediment(km2)'].values.tolist())
-            df_new['date'] = folder_name
-            df = pd.concat([df, df_new])
+            data_temp = nc.variables[dataset][:]
+            data = data_temp.flatten('C')
+            data_split = np.ma.hsplit(data, 1)
+            df_temp = pd.DataFrame(data_split)
+            df_temp['date'] = folder_name
+            df = pd.concat([df, df_temp])
 
-df.to_csv('pd_sediment2.csv', index=False)      
+df.to_csv('pd_all_sediment.csv', index=False) 
+
 
