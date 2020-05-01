f4fec76b5a75c81c8663f7ba0bf9643ed670109e
ailjia <43505441+ailjia@users.noreply.github.com>
2020-05-01 15:18:15 +0200

    Update sediment_all_positions.py

--- data-preprocessing/sediment_all_positions.py
+++ data-preprocessing/sediment_all_positions.py
@@ -25,7 +25,7 @@ def read_data(root_dir = os.listdir(), variable = 'spm_nechad2016', nlats = 2946
         Dimension of the latitudes, for the default the original value of 2718 is used.
     Returns
     -------
-    DataFrame
+    df : DataFrame
         A dataframe with date and sediment values as columns. Each non-date column represents sediment in a location. 
         Dates are a list of date strings with format Y%-m%-%d.
         Non-date columns contain the result for the given algorithm for each pixel/location,
