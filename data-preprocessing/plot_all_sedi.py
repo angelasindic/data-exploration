aeafcdba542d59dd297efe2789b2b59c2747f578
ailjia <43505441+ailjia@users.noreply.github.com>
2020-05-01 19:32:17 +0200

    Update plot_all_sedi.py

--- data-preprocessing/plot_all_sedi.py
+++ data-preprocessing/plot_all_sedi.py
@@ -3,10 +3,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import datetime as dt
 from sediment_all_positions import read_data
 
-df = read_data(root_dir = os.listdir(), nlats = 2946, nlons = 2718) # read in the csv data that has all the sediment
+root_dir = '/data/results/batch_run'
+#df = read_data(root_dir, variable = 'spm_nechad2016', nlats = 2946, nlons = 2718)
 
 def restrain_area(df, threshold_water = 5, threshold_land = 80):
   """
@@ -47,39 +47,6 @@ def restrain_area(df, threshold_water = 5, threshold_land = 80):
   df_relevant = df.drop(columns_to_drop, axis = 1)  
 
   return df_relevant
-
-
-
-def add_dates(df):
-    """
-    Summary line.
-    Extended description of function.
-    Parameters
-    ----------
-    df : DataFrame
-        a dataframe that contains date and each position with the sediment value as the column
-    Returns
-    -------
-    result : DataFrame
-        A dataframe with all dates with an interval of 5 days --> frequency of sentinel-2 satellite imagery
-    """
-    
-    df['DateTime'] = pd.to_datetime(df['date']) # make sure that this dateframe has bee sorted according to datetime
-    df = df.drop(['date'], axis=1).sort_values(["DateTime"], ascending = (True))
-
-    no_circles = int((df['DateTime'].iloc[-1] - df['DateTime'].iloc[1]) / dt.timedelta(days=5)) + 1
-
-    all_time = []
-    for i in range(no_circles):
-        temp = df['DateTime'].iloc[1] + i * dt.timedelta(days=5)
-        all_time.append(temp)
-        df_new = pd.DataFrame(all_time, columns =['DateTime'])
-
-    df_all_dates = pd.merge(df_new, df, on='DateTime', how = 'left') 
-    
-    return df_all_dates
-
-#df_all_dates.to_csv('sediment_per_location_over_all_time.csv')
 
 
 def plot_positions(df_all_dates): # year = '', location = []  # need to add year and location, for demo
