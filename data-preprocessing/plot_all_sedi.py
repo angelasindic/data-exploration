64d847f65cc9c25c07457cfe176a138516c4664b
ailjia <43505441+ailjia@users.noreply.github.com>
2020-05-01 10:44:24 +0200

    Create plot_all_sedi.py

---
+++ data-preprocessing/plot_all_sedi.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('pd_all_sediment.csv') # read in the csv data that has all the sediment
+
+select_col = [col for col in df.columns if col != 'date']
+
+df.plot(x='date', y= select_col, figsize=(10,5), grid=True)
