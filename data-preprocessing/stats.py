c89d0852be98076b9374154da61f66d40e4c8163
ailjia <43505441+ailjia@users.noreply.github.com>
2020-05-01 11:30:51 +0200

    Update stats.py

--- data-preprocessing/stats.py
+++ data-preprocessing/stats.py
@@ -11,6 +11,7 @@ from PIL import Image
 df = pd.read_csv('pd_all_sediment.csv')
 
 df1 = df.drop(['date'], axis=1)
+
 df2 = df1.mean(axis = 0, skipna=True)
 
 df3 = df1.std(axis = 0, skipna=True)
@@ -23,5 +24,7 @@ df_total['std'] = df3
 
 percent_missing = df1.isnull().sum() * 100 / len(df1)
 df_total['missing'] = percent_missing
+
+df_total.loc[df_total['missing'] > 80, 'mean'] = float('nan') # if the percentage of NaN higher than 80%, then mean is NaN
 
 df_total.to_csv('df_mean_std_missing', index = False)
